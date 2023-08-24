11fbd07a805ef4855dae4c427fc12ab727104f74
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2023-08-24 17:59:12 +0100

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.github.com/maryagiamah/alx-low_level_programming/master/0x18-dynamic_libraries/hack.so
+wget -P .. https://github.com/maryagiamah/alx-low_level_programming/blob/20cba0427230b2d762654b31399c666a64c4c5e9/0x18-dynamic_libraries/hack.so
 export LD_PRELOAD=../hack.so
