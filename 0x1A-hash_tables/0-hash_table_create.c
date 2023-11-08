33af61bfe46699601a4eb2f0777066c9b663ffde
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2023-11-09 00:21:30 +0100

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,6 +8,8 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
         /* create new hash table */
+        hash_table_t* table;
+        
         hash_table_t* table = (hash_table_t*) malloc(sizeof(hash_table_t));
         if (!table)
                 return NULL;
