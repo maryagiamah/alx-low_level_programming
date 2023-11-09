c8a48779fc100014303ea9d82b37915852d630f3
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2023-11-09 02:52:42 +0100

    Update 4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -20,7 +20,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
                         if (strcmp(current->key, key) == 0)
                         {
                                 
-                                return current->key->value;
+                                return current->value;
                         }
 
                         current = current->next;
