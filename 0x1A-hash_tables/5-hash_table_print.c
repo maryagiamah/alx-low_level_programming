95c03b880532dd6e298f724f903afeb4d3d85de8
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2023-11-09 05:07:46 +0100

    Create 5-hash_table_print.c

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+/**
+ *  hash_table_print  - prints a hash table.
+ * @ht: double pointer to a linked list
+ *
+ * Return: void. Print the key/value in the order that they appear in the array of hash table.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+    int i;
+    hash_node_t *current;
+
+    if (!ht)
+    {
+        printf("{}\n");
+        return;
+    }
+
+    printf("{");
+    for (i = 0; i < ht->size; i++)
+    {
+        current = ht->array[i];
+        while (current)
+        {
+            if (current->next == NULL)
+                printf("'%s':'%s'", current->key, current->value);
+            else
+                printf("'%s':'%s', ", current->key, current->value);
+
+            current = current->next;
+        }
+    }
+    printf("}\n");
+}
