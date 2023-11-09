71a0773b18d04b85af4987a0870aa7344fff501c
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2023-11-09 05:23:24 +0100

    Update hash_tables.h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -36,4 +36,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 hash_node_t *add_node(hash_node_t **head, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
+void hash_table_print(const hash_table_t *ht);;wq
+
 #endif /* MAIN_H */
