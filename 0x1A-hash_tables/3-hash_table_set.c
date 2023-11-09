e037b549a829442abf5774f5553c73bd2df00614
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2023-11-09 02:25:12 +0100

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -53,7 +53,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
                 new = add_node(&(ht->array[index]), key, value);
                 if(!new)
                         return 0;
-                        ht->array[index] = add_node(&(ht->array[index]), key, value);
+                        ht->array[index] = new;
         }
         else
         {
@@ -75,7 +75,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
                                 new = add_node(&(ht->array[index]), key, value);
                                 if(!new)
                                         return 0;
-                                add_node(&(ht->array[index]), key, value);
                                 return 1;
                         }
 
