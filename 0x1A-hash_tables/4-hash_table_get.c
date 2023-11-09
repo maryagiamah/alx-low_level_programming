811b741d38436bc0c8c1c8efa3107443d8a2c96a
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2023-11-09 02:51:39 +0100

    Update 4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,7 +12,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
         hash_node_t *current;
         unsigned long int index = key_index((const unsigned char *)key, ht->size);
-        if (*ht)
+        if (ht)
                current = ht->array[index];
 
         while (current)
@@ -20,7 +20,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
                         if (strcmp(current->key, key) == 0)
                         {
                                 
-                                return key->value;
+                                return current->key->value;
                         }
 
                         current = current->next;
