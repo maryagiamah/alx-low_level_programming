2ccbabb841aa044b1c6b51a68ad7c681ba63030d
maryagiamh <maryagiamh@gmail.com>
2023-11-29 03:01:27 -0800

    update it

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,12 +9,13 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t i;
-
-	for (i = 0; h != NULL; i++)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-	}
-	return (i);
+    size_t count = 0;
+    dlistint_t *current = (dlistint_t *)h;
+    while(current)
+    {
+        printf("%d\n", current->n);
+        current = current->next;
+        count++;
+    }
+    return count;
 }
