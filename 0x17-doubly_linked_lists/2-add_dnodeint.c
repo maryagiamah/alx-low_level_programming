e5b5465c5ff32e34123bb680e70ec8b587d34411
maryagiamh <maryagiamh@gmail.com>
2023-11-29 03:38:41 -0800

    you

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -17,8 +17,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	}
 	new->n = n;
-	new->next = *head;
 	new->prev = NULL;
+	new->next = *head;
 	*head = new;
+	if (*head)
+	{
+		(*head)->prev = new;
+	}
 	return (new);
 }
