b37cec8aa8fac184cb0196afac4d1d37f55f1e14
Under20techie <under20techie@gmail.com>
2023-12-16 19:23:08 +0100

    fixed betty issues

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,14 +9,14 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-        /* create new hash table */
-        hash_table_t *table = (hash_table_t *) malloc(sizeof(hash_table_t));
+	/* create new hash table */
+	hash_table_t *table = (hash_table_t *) malloc(sizeof(hash_table_t));
 
-        if (!table)
-                return (NULL);
-        table->size = size;
-        table->array = (hash_node_t **) malloc(table->size * sizeof(hash_node_t *));
-        if (!table->array)
-                return (NULL);
-        return (table);
+	if (!table)
+		return (NULL);
+	table->size = size;
+	table->array = (hash_node_t **) malloc(table->size * sizeof(hash_node_t *));
+	if (!table->array)
+		return (NULL);
+	return (table);
 }
