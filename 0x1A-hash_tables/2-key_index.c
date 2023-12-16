31741a000899305834d0078a4dc5600649d1c449
Under20techie <under20techie@gmail.com>
2023-12-16 19:29:35 +0100

    fixed betty issues in 2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -5,11 +5,12 @@
  * @size: size of the array.
  *
  * @key: The key, string
- * Return: The index at which the key/value pair should be stored in the hash table
+ * Return: The index of key/value pair in the hash table
  */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-        unsigned long int index = hash_djb2((unsigned char *)key);
-        return index % size;
+	unsigned long int index = hash_djb2((unsigned char *)key);
+
+	return (index % size);
 }
