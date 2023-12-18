3c2ae50d0c5716212ab1491d34d98d432dc0cc91
Under20techie <under20techie@gmail.com>
2023-12-18 12:30:10 +0100

    REdid 6-hash_table_delete.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,14 +9,19 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	hash_table_t *table = NULL;
+
 	/* create new hash table */
-	hash_table_t *table = (hash_table_t *) malloc(sizeof(hash_table_t));
+	table = (hash_table_t *) malloc(sizeof(hash_table_t));
 
 	if (!table)
 		return (NULL);
 	table->size = size;
 	table->array = (hash_node_t **) malloc(table->size * sizeof(hash_node_t *));
 	if (!table->array)
+	{
+		free(table);
 		return (NULL);
+	}
 	return (table);
 }
--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -9,22 +9,30 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-        usigned long int i;
-        hash_table_t *current
-        for (i = 0; i < ht->size; i++)
-        {
-                current = ht->array[i];
-                while (current)
-                {
-                        free(current->key);
-                        free(current->value);
-                        free(current);
-                        current = current->next;
-                }
-        }
-        if (ht)
-        {
-                free(ht->array);
-                free(ht);
-        }
+	unsigned long int i;
+	hash_node_t *current, *next;
+
+	if (!ht || !ht->array)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		if (current)
+		{
+			while (current)
+			{
+				next = current->next;
+				if (current->key)
+					free(current->key);
+				if (current->value)
+					free(current->value);
+				if (current)
+					free(current);
+				current = next;
+			}
+			ht->array[i] = NULL;
+		}
+	}
+	free(ht->array);
+	free(ht); 
 }
