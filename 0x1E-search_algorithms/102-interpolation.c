fbd3d31f4455d3c06c85504b4be59ee6b9565564
Your Name <you@example.com>
2024-02-07 13:49:22 +0100

    you

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+
+/**
+ * interpolation_search - uses binary search algorithm on an array
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: the value to search for
+ *
+ * Return: ZERO OR VALUE INDEX
+ */
+int interpolation_search(int *array, size_t size, int value)
+{                                                                                              size_t left = 0, right = size - 1, mid;
+
+	if (!array)                                                                                     return (-1);
+	while (left <= right)
+	{                                
+		mid = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+		if (array[mid] < value)
+			left = mid + 1;
+		else if (array[mid] > value)                                                                    right = mid - 1;                                                                else
+			return (mid);
+	}
+	return (-1);
+
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,5 +7,6 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, size_t left, size_t right);
 int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
 int search(int *array, size_t begin, size_t end, int value);
 #endif /* SEARCH_ALGOS_H */
