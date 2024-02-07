a60718b53a3b4b55670df57adf611891e651c1ab
Your Name <you@example.com>
2024-02-07 03:55:57 +0100

    you

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -41,7 +41,7 @@ int jump_search(int *array, size_t size, int value)
 	for (i = 0; i < size; i += step)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		if (value < (i + step))
+		if (value < (int)(i + step))
 		{
 			printf("Value found between indexes [%ld] and [%ld]\n", i, i + step);
 			index = linear_search(array + i, i + step, value);
