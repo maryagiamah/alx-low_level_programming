e5a7b584daf4010ccae85ca37e34e941305ca576
Your Name <you@example.com>
2024-02-07 14:08:22 +0100

    you

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -14,7 +14,7 @@ int interpolation_search(int *array, size_t size, int value)
 	if (!array)                                                                                     return (-1);
 	while (left <= right)
 	{                                
-		mid = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+		mid = left + (((double)(right - left) / (array[right] - array[left])) * (value - array[left]));
 		if (array[mid] < value)
 			left = mid + 1;
 		else if (array[mid] > value)                                                                    right = mid - 1;                                                                else
