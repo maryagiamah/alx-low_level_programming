7b4b33e3062a15105367c70a2d5384c58fda0e37
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2024-07-18 17:03:43 +0100

    Update 103-exponential.c

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -78,6 +78,8 @@ int exponential_search(int *array, size_t size, int value)
 		return (-1);
 	if (array[0] == value)
 		printf("Value checked array[%d] = [%d]\n", 0, array[0]);
+		return (0);
+
 	for (i = 1; i < size; i *= step)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
